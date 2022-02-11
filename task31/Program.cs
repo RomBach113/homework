ca3bd3f4c433cde92d2309b18290b731c831ecfc
RomBach113 <rimanbachinsky113@gmail.com>
2022-02-11 13:53:34 +0300

    task 31

---
+++ task31/Program.cs
@@ -0,0 +1,13 @@
+﻿//31. задатьмассив из 8 элементов и вывести их на экран
+//int[] a={1,2,3,4,5,6,7,8,9};
+
+//System.Console.WriteLine(a[0]);
+//System.Console.WriteLine(a[9]);
+
+double[] a=new double[20];
+
+for(int i=0;i<a.Length;i++)
+a[i]=new Random().Next(0,100);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i]} ");
