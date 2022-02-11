ae7417354280e923695c104e1967c15466d6b4e1
RomBach113 <rimanbachinsky113@gmail.com>
2022-02-11 12:27:35 +0300

    task 2

--- intro_cs/Program.cs
+++ intro_cs/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a=5,b=5;
+//if(a>b) Console.WriteLine("a больше b");
+//else Console.WriteLine("a не большеb");
+if(a>b) Console.WriteLine("a больше b");
+if(a<b) Console.WriteLine("a не больше b");
+if(a==b) Console.WriteLine("a равно b");
